bbd83473fbf4b2dc708e4ee3371790ba4626236a
Nand Yaduwanshi <gccboys887@gmail.com>
2025-03-26 21:24:52 +0530

    •─╼⃝𖠁 𝐍ᴀɴᴅ 𝐘ᴀᴅᴜᴡᴀɴsʜɪ 𖠁⃝╾─•

--- config.py
+++ config.py
@@ -41,15 +41,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MesteriousPrivate/ChatMusic",
+    "https://github.com/powerfulboys123/NoxxMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/heaven_of_actresses")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+XwY-WRKaWhhlYzk1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NoxxNetwork")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NYCreation_Chatzone")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
